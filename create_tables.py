d135a128b33e2f93eae3807ef4c710896d25ab24
Fan Yuan <yfan941101@gmail.com>
2021-03-09 21:34:03 -0500

    Update: Add function doc to create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,28 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drop any tables if exists before running creating script
+    
+    Args:
+        cur: cursor object to execute PostgreSQL command through Python
+        conn: connection to database
+    """
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Create tables in database using the CREATE queries in sql_queries.py
+    
+    Args:
+        cur: cursor object to execute PostgreSQL command through Python
+        conn: connection to database
+    """
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
